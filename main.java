dfd0ed054f263c05e79a8205ec387a4425ff9f6c
kondrotM <kondrot.m@gmail.com>
2019-11-09 16:09:36 +0000

    Added comments need validation for 'create': > can't create team if there is no tournament > can't create player if there are no teams

--- main.java
+++ main.java
@@ -1,21 +1,30 @@
 package uk.ac.glos.ct5025.s1804317.footballStats;
 
-import java.util.AbstractCollection;
+// ArrayList necessary for declaring arrays
 import java.util.ArrayList;
+// Scanner necessary for getting user input
 import java.util.Scanner;
+
+// main class to allow user interface interaction
 class main {
-    //private static AbstractCollection<Tournament> tournamentList;
+    // list of tournaments declared and initialised. all tournaments, teams and players are declared in regards to the tournament list
     public static ArrayList<Tournament> tournamentList = new ArrayList<Tournament>();
+    // actively selected tournament
     public static Tournament activeTournament;
 
+    // method to get user input
     public static String getInput(String prompt){
+
+        // prints given prompt so long as there is one
         if (prompt != null) {
             System.out.println(prompt);
         }
+        // checks what's written on the next line and returns it
         Scanner cmd = new Scanner(System.in);
         return cmd.nextLine();
     }
 
+    // method to print main menu
     public static void getMainMenu(){
         System.out.println("FOOTBALL STATS");
         System.out.println("MAIN MENU");
@@ -25,44 +34,59 @@ class main {
         System.out.println("4. Select Tournament");
     }
 
+    // selects active tournament
     public static void selectTournament(){
         System.out.println("Current active tournament: " + activeTournament.getTournamentName());
+        // lists all available tournaments
         for (int i = 0; i < tournamentList.size(); i++) {
             Tournament currTournament = tournamentList.get(i);
             System.out.println(i+1 + ". " + currTournament.getTournamentName());
         }
+        // gets user input and sets it as active tournament
         String currTournament = getInput("Select team number");
         int tournamentNumber = Integer.parseInt(currTournament);
         activeTournament = tournamentList.get(tournamentNumber-1);
     }
 
+    // creates player and assigns them to a team
     public static void createPlayer(){
+        // lists all teams within selected tournament
         ArrayList currTeams = activeTournament.getTournamentTeams();
         for (int i = 0; i < currTeams.size(); i++) {
             Team currTeam = (Team) currTeams.get(i);
             System.out.println(i+1 + ". " + currTeam.getTeamName());
         }
+        // asks user for selected team number
         String teamTempNo = getInput("Enter team number");
         int teamNo = Integer.parseInt(teamTempNo);
 
+        // asks user for player information
         String playerName = getInput("Enter player name");
         String playerDoB = getInput("Enter player DoB");
         Player player1 = new Player(playerName, playerDoB);
+
+        // gets selected team from active tournament
         Team currTeam = activeTournament.getTeam(teamNo-1);
+
+        // assigns player to selected team
         currTeam.addPlayer(player1);
         System.out.println(player1.getPlayerName());
     }
 
+        // creates team within active tournament
     public static void createTeam(){
         String teamName = getInput("Enter team name");
         Team team1 = new Team(teamName);
         activeTournament.addTeam(team1);
     }
 
+        // creates new tournament
     public static void createTournament(){
         String tournamentName = getInput("Enter tournament name");
+        // creates new tournament and appends it to the tournament list
         Tournament tournament1 = new Tournament(tournamentName);
         tournamentList.add(tournament1);
+        // if there is only one tournament, it is set as the active one
         if(tournamentList.size()==1) {
             activeTournament = tournamentList.get(0);
         }
